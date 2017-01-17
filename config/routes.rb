0a0fc0bebd6da21293dd5da711caff4faa54e9b1
dscace <derek.scace@gmail.com>
2017-01-17 11:52:38 -0700

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+	root 'blog_posts#index'
   resources :blog_posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
