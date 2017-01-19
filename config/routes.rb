a91d9bf3d648d2ff3dd6da69f05e9180d4764de3
dscace <derek.scace@gmail.com>
2017-01-19 14:14:07 -0700

    Non users can now view root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :comments
+  
 	root 'blog_posts#index'
+	get 'index' => 'blog_posts#index'
+
   resources :blog_posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
